73620befeea5c7d4f4cb27ca00940d546a70207e
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-12-30 13:17:08 +0100

    added resources for Creating More Components

---
+++ 01-react-novice/03-working-with-components-props-jsx/06-creating-more-components/resources/src/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return (
+    <div>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+const pizzaData = {
+  name: "Delicious Spinach Pizza",
+  ingredients: "spinach, mozzarella, tomatoes"
+};
+
+function Pizza() {
+  return (
+    <div>
+      <h2>{pizzaData.name}</h2>
+      <p>{pizzaData.ingredients}</p>
+      <img src="spinaci.jpg" alt={pizzaData.name} />
+    </div>
+  );
+}
+
+// React v18
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// React before 18
+// ReactDOM.render(<App />, document.getElementById("root"));
