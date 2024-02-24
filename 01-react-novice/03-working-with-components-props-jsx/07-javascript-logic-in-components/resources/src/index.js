1926dc2f3fc89b771f7607466695565aacbcdd5d
Minh Hung Phan <minhhung.phan2701@gmail.com>
2024-02-24 23:13:20 +0100

    added resources for JavaScript Logic in React Components

---
+++ 01-react-novice/03-working-with-components-props-jsx/07-javascript-logic-in-components/resources/src/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+function Footer() {
+  return <footer>{new Date().toLocaleTimeString()}. We're currently open!</footer>;
+}
+
+const pizzaData = {
+  name: "Delicious Spinach Pizza",
+  ingredients: "spinach, mozzarella, tomatoes"
+};
+
+function Pizza() {
+  return (
+    <div>
+      <h2>{pizzaData.name}</h2>
+      <p>{pizzaData.ingredients}</p>
+      <img src="spinaci.jpg" alt={pizzaData.name} />
+    </div>
+  );
+}
+
+// React v18
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// React before 18
+// ReactDOM.render(<App />, document.getElementById("root"));
